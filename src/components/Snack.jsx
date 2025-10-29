86a73695b36501d3b890bbfde01e94b5db777efc
b0nLock <b1n13eaker@gmail.com>
2025-10-29 15:31:19 +0300

    MUI page

---
+++ src/components/Snack.jsx
@@ -0,0 +1,12 @@
+import { Alert, Snackbar } from '@mui/material'
+import React from 'react'
+
+const Snack = ({isOpen, handleClose}) => {
+  return (
+    <Snackbar open={isOpen} onClose={handleClose} autoHideDuration={3000}>
+        <Alert security="success">Товар добавлен в корзину</Alert>
+    </Snackbar>
+  )
+}
+
+export default Snack
