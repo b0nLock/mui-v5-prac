c6815bef654cd998c4bfe90a575580d3c41ecebd
b0nLock <b1n13eaker@gmail.com>
2025-10-30 14:50:19 +0300

    prettier + eslint

--- src/components/BasketItem.jsx
+++ src/components/BasketItem.jsx
@@ -1,17 +1,17 @@
-import { Close } from "@mui/icons-material";
-import { IconButton, ListItem, Typography } from "@mui/material";
+import { Close } from '@mui/icons-material';
+import { IconButton, ListItem, Typography } from '@mui/material';
 
-const BasketItem = ({removeFromOrder, id, name, price, quantity}) => {
-    return (
-        <ListItem>
-            <Typography variant="body1">{name} {price}руб x{quantity}</Typography>
-            <IconButton
-                onClick={() => removeFromOrder(id)}
-            >
-               <Close />
-            </IconButton>
-        </ListItem>
-    );
+const BasketItem = ({ removeFromOrder, id, name, price, quantity }) => {
+  return (
+    <ListItem>
+      <Typography variant="body1">
+        {name} {price}руб x{quantity}
+      </Typography>
+      <IconButton onClick={() => removeFromOrder(id)}>
+        <Close />
+      </IconButton>
+    </ListItem>
+  );
 };
 
 export default BasketItem;
--- src/components/GoodsList.jsx
+++ src/components/GoodsList.jsx
@@ -4,15 +4,15 @@ import GoodsItem from './GoodsItem';
 import { Grid } from '@mui/material';
 
 const GoodsList = (props) => {
-    const { goods, setOrder } = props;
+  const { goods, setOrder } = props;
 
-    return (
-        <Grid container spacing={2}>
-            {goods.map((item) => (
-                <GoodsItem key={item.id} setOrder={setOrder} {...item} />
-            ))}
-        </Grid>     
-    );
+  return (
+    <Grid container spacing={2}>
+      {goods.map((item) => (
+        <GoodsItem key={item.id} setOrder={setOrder} {...item} />
+      ))}
+    </Grid>
+  );
 };
 
 export default GoodsList;
